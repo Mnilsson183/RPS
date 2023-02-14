c4746e1315c57e9f0ec30d43aa826a0425a3fd40
Mnilsson183 <morgan.nilsson7111@icloud.com>
2023-02-14 11:11:53 -0700

    randomize and feed forward

--- brain.js
+++ brain.js
@@ -41,6 +41,50 @@ class level
             this.weights[i] = new Array(outputCount);
         }
 
-        level.#randomize(this)
+        level.#randomize(this);
     }
+
+    static #randomize(level)
+    {
+        for (let i = 0; i < level.inputs.length; i++)
+        {
+            for (let j = 0; j < level.outputs.length; j++)
+            {
+                level.weights[i][j] = Math.random() * 2 - 1;
+            }
+        }
+        for (let i = 0; i < level.biases.length;i++)
+        {
+            level.biases[i] = Math.random() * 2 - 1;
+        }
+    }
+
+
+    static feedForward(inputs, level)
+    {
+        for (let i = 0; i < level.inputs.length; i++)
+        {
+            level.inputs[i] = inputs[i];
+        }
+
+        for (let i = 0; i < level.outputs.length; i++)
+        {
+            let sum = 0;
+            for (let j = 0; j < level.inputs.length; j++)
+            {
+                sum += level.inputs[j] * level.weights[j][i];
+            }
+
+            if (sum > level.biases[i])
+            {
+                level.outputs[i] = 1;
+            }
+            else{
+                level.outputs[i] = 0;
+            }
+        }
+
+        return level.outputs;
+    }
+
 }
