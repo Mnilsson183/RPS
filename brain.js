6d1f44ca7f4c183ad204acb8a9f7699919d861f0
Morgan Nilsson <morgan.nilsson7111@icloud.com>
2023-02-15 08:29:47 -0700

    commit

--- brain.js
+++ brain.js
@@ -11,11 +11,7 @@ class brain
         const offsets = this.sensor.readings.map(
         )
                 // offsets is the values of each value
-<<<<<<< HEAD
         const outputs = NeuralNetwork.feedForward(input.share(),this.brain);
-=======
-        const outputs = NeuralNetwork.feedForward(,this.brain);
->>>>>>> ffbbdbd28266cf81b3d6f49053d5c1284c87aed4
             
             this.rock = outputs[0];
             this.paper = outputs[1];
