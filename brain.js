ffbbdbd28266cf81b3d6f49053d5c1284c87aed4
Mnilsson183 <morgan.nilsson7111@icloud.com>
2023-02-14 19:34:29 -0700

    message

--- brain.js
+++ brain.js
@@ -6,13 +6,13 @@ class brain
             [12,8,3]
         )
     }
-    update(roadBorders,traffic){
-
+    idk()
+    {
         const offsets = this.sensor.readings.map(
             s=>s==null?0:1-s.offset
         )
                 // offsets is the values of each value
-        const outputs = NeuralNetwork.feedForward(offsets,this.brain);
+        const outputs = NeuralNetwork.feedForward(,this.brain);
             
             this.rock = outputs[0];
             this.paper = outputs[1];
